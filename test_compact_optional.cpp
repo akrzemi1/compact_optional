7f50c923e087f080536e53c1ab2526d095d0a608
Andrzej Krzemieński <akrzemi1@gmail.com>
2015-07-10 12:50:50 +0200

    testing support for optional

--- test_compact_optional.cpp
+++ test_compact_optional.cpp
@@ -18,6 +18,8 @@ void test_value_ctor()
 {
   {
     typedef compact_optional< int, empty_scalar_value<int, -1> > opt_int;
+    static_assert (sizeof(opt_int) == sizeof(int), "size waste");
+    
     opt_int oi_, oiN1(-1), oi0(0), oi1(1);
     assert (!oi_.has_value());
     assert (!oiN1.has_value());
@@ -29,6 +31,7 @@ void test_value_ctor()
   }
   {
     typedef compact_optional< int, empty_scalar_value<int, 0> > opt_int;
+    static_assert (sizeof(opt_int) == sizeof(int), "size waste");
     opt_int oi_, oiN1(-1), oi0(0), oi1(1);
     assert (!oi_.has_value());
     assert ( oiN1.has_value());
@@ -40,7 +43,7 @@ void test_value_ctor()
   }
 }
 
-struct string_empty_value
+struct string_empty_value : compact_optional_type<std::string>
 {
   static std::string empty_value() { return std::string("\0\0", 2); }
   static bool is_empty_value(const std::string& v) { return v == std::string("\0\0", 2); }
@@ -49,6 +52,8 @@ struct string_empty_value
 void test_string_traits()
 {
   typedef compact_optional<std::string, string_empty_value, class tag_X> opt_str;
+  static_assert (sizeof(opt_str) == sizeof(std::string), "size waste");
+  
   opt_str os_, os00(std::string("\0\0", 2)), os0(std::string("\0")), osA(std::string("A"));
   assert (!os_.has_value());
   assert (!os00.has_value());
@@ -56,8 +61,39 @@ void test_string_traits()
   assert ( osA.has_value());
 }
 
+struct string_in_pair_empty_val : compact_optional_type< std::string, std::pair<bool, std::string> >
+{
+  static storage_type empty_value() { return storage_type(false, "anything"); }
+  static bool is_empty_value(const storage_type& v) { return v.first == false; }
+  
+  static const value_type& access_value(const storage_type& v) { return v.second; }
+  static storage_type store_value(const value_type& v) { return storage_type(true, v); }
+  static storage_type store_value(value_type&& v) { return storage_type(true, std::move(v)); }
+};
+
+void test_custom_storage()
+{
+  typedef compact_optional<std::string, string_in_pair_empty_val> opt_str;
+  opt_str os_, os0(std::string("\0")), osA(std::string("A"));
+  
+  assert (!os_.has_value());
+  
+  assert (os0.has_value());
+  assert (os0.value() == "");
+  
+  assert (osA.has_value());
+  assert (osA.value() == "A");
+
+  swap (os_, os0);
+    
+  assert (os_.has_value());
+  assert (os_.value() == "");
+  assert (!os0.has_value());
+}
+
 int main()
 {
   test_value_ctor();
   test_string_traits();
+  test_custom_storage();
 }
